066b1a05d7ef25788907132bd3750f3ee538467e
Arpita Saha <arpita.sahaa@gmail.com>
2021-06-06 10:30:14 -0400

    added delete feature

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Todo({item, todoList, setTodoList}) {
+function Todo({ item, todoList, setTodoList }) {
 
-    const toggleBtn = (e) =>{
+    const toggleBtn = (e) => {
         let newList = todoList.map(el => {
-            if (el.id === item.id){
-                return {...el, compStatus : !el.compStatus}
+            if (el.id === item.id) {
+                return { ...el, compStatus: !el.compStatus }
             }
             else {
                 return el
@@ -14,10 +14,20 @@ function Todo({item, todoList, setTodoList}) {
         setTodoList(newList)
     }
 
+    const deleteItem = () => {
+        console.log("hey");
+        let listAfterDelete = todoList.filter(el => {
+            return el.id != item.id
+
+        })
+        setTodoList(listAfterDelete);
+    }
+
     return (
         <div>
-            <h3 className ={item.compStatus === true ? "strikeOut" : ""}> {item.text} </h3>
-            <button onClick={toggleBtn} type="button">toggle</button>
+            <h3 className ={item.compStatus ? "strikeOut" : ""}> {item.text} </h3>
+            <button onClick={toggleBtn} type="button">Toggle</button>
+            <button onClick={deleteItem} type="button">Delete</button>
         </div>
     )
 }
