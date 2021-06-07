b721257456e3c914b8eaabedfd639036d71e055d
Arpita Saha <arpita.sahaa@gmail.com>
2021-06-07 17:19:23 -0400

    added local storage functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
-import './App.css';
+import "./App.css";
 import TodoList from "./components/TodoList";
 
 function App() {
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
   const [status, setStatus] = useState("All");
- 
+  const [filteredList, setFilteredList] = useState([]);
+
+  const filterResult = () => {
+    if (status === "Completed") {
+      setFilteredList(
+        todoList.filter((el) => {
+          return el.compStatus === true;
+        })
+      );
+    } else if (status === "Incomplete") {
+      setFilteredList(
+        todoList.filter((el) => {
+          return el.compStatus === false;
+        })
+      );
+    } else {
+      setFilteredList(todoList);
+    }
+  };
+
+  useEffect(() => {
+    filterResult();
+  }, [status, todoList]);
+
+  useEffect(() => {
+    localStorage.setItem("todoListkey", JSON.stringify(todoList));
+  }, [todoList]);
+
+  useEffect(() => {
+    if (localStorage.getItem("todoListkey") != null) {
+      setTodoList(JSON.parse(localStorage.getItem("todoListkey")));
+    }
+  }, []);
 
   return (
-    <div >
+    <div>
       <h1>Hello</h1>
-      <Form todo={todo} setTodo={setTodo} todoList={todoList} setTodoList={setTodoList} setStatus={setStatus} />
-      <TodoList  setTodoList={setTodoList} todoList={todoList} />
-    </div >
+      <Form
+        todo={todo}
+        setTodo={setTodo}
+        todoList={todoList}
+        setTodoList={setTodoList}
+        setStatus={setStatus}
+      />
+      <TodoList setTodoList={setTodoList} todoList={filteredList} />
+    </div>
   );
 }
 
